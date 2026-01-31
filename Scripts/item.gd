0d00a464e63f781e434f5edd5095e99e10d40b3d
andreviews <andreviews@proton.me>
2026-01-31 10:17:50 -0800

    can move items around

--- Scripts/item.gd
+++ Scripts/item.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-@export var settle: int = 1
-@export var fall: int = 2
+@export var settle: int = 2 #Initial drop
+@export var fall: int = 25 #Fall speed
+@export var follow_cursor: int = 120 #Speed you can move them
 
 var stop: float
+var held: bool = false
+var hold_point: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +15,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if held:
+		var mouse = get_viewport().get_mouse_position()
+		set_position(position + (mouse - position).normalized() * follow_cursor * delta)
 	if position.y < stop:
 		set_position(position + Vector2.DOWN * fall * delta)
 
@@ -21,3 +27,13 @@ func initialize(pos: Vector2) -> void:
 	stop = pos.y + settle
 	
 	#TODO: randomize item sprite
+
+
+func _on_color_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			held = true
+			hold_point = get_viewport().get_mouse_position()-position
+		else:
+			held = false
+			#TODO: Set new stop
